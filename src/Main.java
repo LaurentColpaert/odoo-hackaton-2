878c303403479041ee7184d3f5ef792b8764429c
amezzo99 <bouchouariahmed@gmail.com>
2023-04-01 02:05:56 +0200

    Initiate git

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Main {
+    public static void connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:C:\\Users\\Ahmed\\Desktop\\HACKITOS\\little_step.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        connect();
+    }
+}
